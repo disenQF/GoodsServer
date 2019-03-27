e3e0faf0d2fec520dd394cbb725c78e240df3237
disen <610039018@qq.com>
2019-03-27 17:17:04 +0800

    add 优惠劵 详情

--- apps/quan/adminx.py
+++ apps/quan/adminx.py
@@ -1,7 +1,7 @@
 import xadmin
 from xadmin import views
 
-from quan.models import QaunActive
+from quan.models import QaunActive, QuanInfo
 # Register your models here.
 class QaunActiveAdmin():
     list_display = ['title',
@@ -9,6 +9,10 @@ class QaunActiveAdmin():
                     'publish_date',
                     'valid_days']
 
+class QuanInfoAdmin():
+    list_display = ('title', 'active', 'rate', 'quan_num')
+
 
 # 将模型类 添加到 后台显示， 并且给出定制显示的类
 xadmin.site.register(QaunActive,  QaunActiveAdmin)
+xadmin.site.register(QuanInfo, QuanInfoAdmin)
